6bc439eeb85a906dd956514f5f08768d79029dc7
Agumon <60151244+hase1998@users.noreply.github.com>
2025-06-28 23:46:20 +0900

    Commit: Completed Project
    ne

---
+++ Assets/Scripts/UI/MainManager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.IO;
+
+public class MainManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public static MainManager Instance;
+    public Color TeamColor;
+    
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(this);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        LoadColor();
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public Color TeamColor;
+    }
+
+    public void SaveColor()
+    {
+        SaveData data = new SaveData();
+        data.TeamColor = TeamColor;
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadColor()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+            TeamColor = data.TeamColor;
+        }
+    }
+
+}
